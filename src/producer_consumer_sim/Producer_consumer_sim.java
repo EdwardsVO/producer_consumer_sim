eed05d0d89470337e1b7a3db85c99d810d70a1e7
Edward <edwards.svo@gmail.com>
2021-05-22 11:35:22 -0400

    Intial Commit

---
+++ src/producer_consumer_sim/Producer_consumer_sim.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package producer_consumer_sim;
+
+/**
+ *
+ * @author Edward Vergel
+ */
+public class Producer_consumer_sim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
